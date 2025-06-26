2399a96afdbddcaee93419e293cdc7195694bc80
Justxt <jostinmatt@gmail.com>
2025-06-26 00:39:14 -0500

    API_BASE_URL Railway

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://minicore-8hdi.onrender.com";
+const API_BASE_URL = "minicore-production.up.railway.app";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
